4e35f04979fac423bb67b1d416eaa6e78e3aa9ad
Michael MacCallum <MacCDevTeam@gmail.com>
2013-11-15 23:06:22 -0500

    Removed the need to store the expanded index in a property. Some I managed to completely overlook the fact that I could let the table track that for me and access it through its `indexPathForSelectedRow`

--- ExpandingTableCell/ViewController.m
+++ ExpandingTableCell/ViewController.m
@@ -4,8 +4,6 @@
 
 @interface ViewController () < UITableViewDataSource, UITableViewDelegate >
 
-@property (strong, nonatomic) NSIndexPath *expandedIndexPath;
-
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
 
 @end
@@ -13,26 +11,11 @@
 @implementation ViewController
 
 
-
-
-
-- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+- (void)viewDidLoad
 {
-    [tableView beginUpdates]; // tell the table you're about to start making changes
-
-    // If the index path of the currently expanded cell is the same as the index that
-    // has just been tapped set the expanded index to nil so that there aren't any
-    // expanded cells, otherwise, set the expanded index to the index that has just
-    // been selected.
-
-    if ([indexPath compare:self.expandedIndexPath] == NSOrderedSame) {
-        self.expandedIndexPath = nil;
-    } else {
-        self.expandedIndexPath = indexPath;
-    }
-
-    [tableView endUpdates]; // tell the table you're done making your changes
+    [super viewDidLoad];
 }
+
 
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
 {
@@ -40,15 +23,11 @@
     // index path variable. If the two match, return a height of 100 points, otherwise return
     // a height of 44 points.
 
-    if ([indexPath compare:self.expandedIndexPath] == NSOrderedSame) {
+    if ([indexPath compare:[tableView indexPathForSelectedRow]] == NSOrderedSame) {
         return 100.0; // Expanded height
     }
     return 44.0; // Normal height
 }
-
-
-
-
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
@@ -68,10 +47,18 @@
     return cell;
 }
 
-
-- (void)viewDidLoad
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    [super viewDidLoad];
+    [tableView beginUpdates]; // tell the table you're about to start making changes
+
+    [tableView endUpdates]; // tell the table you're done making your changes
+}
+
+- (void)tableView:(UITableView *)tableView didDeselectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+	[tableView beginUpdates]; // tell the table you're about to start making changes
+
+    [tableView endUpdates]; // tell the table you're done making your changes
 }
 
 - (void)didReceiveMemoryWarning
